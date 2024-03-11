16b3dcb0069c55261f6c5b6224a405817f8aeb7f
userDmytro <104132845+userDmytro@users.noreply.github.com>
2024-03-11 00:35:56 +0000

    Deploying to gh-pages from @ userDmytro/goit-js-hw-09@73e08f65d86e6e6230e79d932b52c72ca05bb7d8 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const n=document.querySelectorAll(".linkList");n.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const r=e.querySelector("a").getAttribute("href");window.location.href=r})});
+//# sourceMappingURL=commonHelpers3.js.map
